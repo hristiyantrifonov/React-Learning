7fd026969b2c41d47d53d91269bf569d3f456efe
Hris <hristiyantrifonov@gmail.com>
2018-07-29 01:02:23 +0100

    States (up to Lecture 43)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import Person from './Person/Person.js';
 
 //The starting React component
 class App extends Component {
+  //Only available where we extend a component (not in function component like Person.js)
+  //We need to be careful with those states (can be unpredictable if many)
+  state = {
+      persons: [
+          {name: 'Max', age: 28},
+          {name: 'Manu', age: 29},
+          {name: 'Stephanie', age: 26}
+      ]
+  }
+
+  //Good practice to name in with handler (because we will use it as event handler)
+  switchNameHandler = () => {
+      // console.log('Was clicked');
+      // DON'T DO THIS this.state.persons[0].name = 'Maximilian'
+      this.setState({persons: [
+              {name: 'Maximilian', age: 28},
+              {name: 'Manu', age: 29},
+              {name: 'Stephanie', age: 17}
+          ]})
+  }
 
   render() { //Every component needs to render some html
     return (
@@ -13,9 +33,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
 
